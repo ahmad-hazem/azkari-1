f361147befbdd3f6f5f6dabad9c553e418cc5fa3
ahmad-hazem <52348040+ahmad-hazem@users.noreply.github.com>
2026-09-24 12:10:54 +0300

    Update build.gradle.kts
    تحديث الاصدار

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,13 +31,15 @@ android {
         applicationId = "com.ahmed.azkari"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
